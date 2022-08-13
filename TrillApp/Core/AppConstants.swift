3f7984b71f997fa76f84f2b1726e48753d0e80ac
Zahidul <z.islam370@gmail.com>
2022-08-13 14:58:18 +0900

    Add app constants

---
+++ TrillApp/Core/AppConstants.swift
@@ -0,0 +1,22 @@
+//
+//  AppConstants.swift
+//  TrillDemo
+//
+//  Created by Zahidul Islam on 2022/08/09.
+//
+
+import Foundation
+
+// Mark: app constant
+class AppConstants {
+    
+    static let emptyString = ""
+    static let okButtonTitle = "Ok"
+    static let NavigationBarTitle = "TRILL"
+    static let landingNavigationBarTitle = "TRILL"
+    static let MainStoryBoard = "Main"
+    static let detailVC = "ArticleDetailsViewController"
+    static let articleDetailSegue = "showDetails"
+    
+    
+}
