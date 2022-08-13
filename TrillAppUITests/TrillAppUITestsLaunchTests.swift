e42d631aadba0603a6254208e8d4cef4d0dbe967
Zahidul <z.islam370@gmail.com>
2022-08-13 14:26:25 +0900

    Initial Commit

---
+++ TrillAppUITests/TrillAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TrillAppUITestsLaunchTests.swift
+//  TrillAppUITests
+//
+//  Created by Zahidul Islam on 2022/08/13.
+//
+
+import XCTest
+
+class TrillAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
