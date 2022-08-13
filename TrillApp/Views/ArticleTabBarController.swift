9aa7add6552460cb37542b3637b5c19cec4d7559
Zahidul <z.islam370@gmail.com>
2022-08-13 15:01:40 +0900

    Add bottom tab navigation

---
+++ TrillApp/Views/ArticleTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleTabBarController.swift
+//  TrillDemo
+//
+//  Created by Zahidul Islam on 2022/08/08.
+//
+
+import UIKit
+
+// Mark: UITabBarController
+class ArticleTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
